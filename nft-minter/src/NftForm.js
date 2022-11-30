10ca41841e35e6b8e801d76d8db3243371d303ac
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-11-30 12:33:02 +0100

    inputs form data into state variable

--- nft-minter/src/NftForm.js
+++ nft-minter/src/NftForm.js
@@ -8,6 +8,18 @@ function NftForm(props) {
 
     const [picture, setPicture] = useState(null);
     const [pictureUrl, setPictureUrl] = useState(null);
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [imageUrlIpfs, setImageUrlIpfs] = useState('');
+    const [propriete1, setPropriete1] = useState('');
+    const [propriete1Description, setPropriete1Description] = useState('');
+
+    const fieldName = useRef(null);
+    const fieldDescription = useRef(null);
+    const fieldImageUrlIpfs = useRef(null);
+    const fieldPropriete1 = useRef(null);
+    const fieldPropriete1Description = useRef(null);
     
     const uploadNftPicture=(e)=>{
         const file = e.target?.files[0];
@@ -15,10 +27,27 @@ function NftForm(props) {
         setPicture(file);
     }
 
+    const handleSend = async()=>{
+        console.log(fieldName.current.value);
+        setName(fieldName.current.value);
+        setDescription(fieldDescription.current.value);
+        setImageUrlIpfs(fieldImageUrlIpfs.current.value);
+        setPropriete1(fieldPropriete1.current.value);
+        setPropriete1Description(fieldPropriete1Description.current.value);
+    }
+
+    useEffect(()=>{
+        console.log(name, description, imageUrlIpfs, propriete1, propriete1Description);
+    },[name, description, imageUrlIpfs, propriete1, propriete1Description]);
+
+    const handleSendPictureToIpfs = async()=>{
+
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '7em', flexWrap: 'wrap' }}>
 
-            <div style={{ width: '45%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '2em', margin: '2em' }}>
+            <form style={{ width: '45%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '2em', margin: '2em' }}>
 
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5em', justifyContent: 'center', alignItems: 'center', marginBottom: '5em' }}>
                     <p style={{ color: 'red' }}>Upload a picture (Format: png/jpg/jpeg)</p>
@@ -28,25 +57,26 @@ function NftForm(props) {
                     </Button>
                 </div>
 
-                <TextField id="outlined-basic" label="Name" variant="outlined" />
-                <TextField id="outlined-basic" label="Description" variant="outlined" />
+                <TextField inputRef={fieldName} id="outlined-basic" label="Name" variant="outlined" />
+                <TextField inputRef={fieldDescription} id="outlined-basic" label="Description" variant="outlined" />
+                <TextField inputRef={fieldImageUrlIpfs} id="outlined-basic" label="ipfs url of the picture" variant="outlined" />
                 <div style={{ display: 'flex', gap: '0.5em' }}>
-                    <TextField id="outlined-basic" label="Propriété1: name" variant="outlined" />
-                    <TextField id="outlined-basic" label="Propriété1: description" variant="outlined" />
-                </div>
-                <div style={{ display: 'flex', gap: '0.5em' }}>
-                    <TextField id="outlined-basic" label="Propriété2: name" variant="outlined" />
-                    <TextField id="outlined-basic" label="Propriété2: description" variant="outlined" />
+                    <TextField inputRef={fieldPropriete1} id="outlined-basic" label="Propriété1: name" variant="outlined" />
+                    <TextField inputRef={fieldPropriete1Description} id="outlined-basic" label="Propriété1: description" variant="outlined" />
                 </div>
 
-                <Button variant="contained" endIcon={<SendIcon />}>
+                <Button onClick={handleSend} variant="contained" endIcon={<SendIcon />}>
                     Send
                 </Button>
-            </div>
+            </form>
 
             <div style={{ width: '45%', display: 'flex', height: '100%', margin: '2em', justifyContent: 'center', alignItems: 'center' }}>
                 <Pictures picture={picture} pictureUrl={pictureUrl} />
             </div>
+
+            {picture && <Button onClick={handleSendPictureToIpfs} variant="outlined" endIcon={<SendIcon />}>
+                    Send the NFT on IPFS
+            </Button>}
 
         </div>
     );
